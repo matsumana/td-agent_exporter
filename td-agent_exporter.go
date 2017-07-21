ad94f6caa7385ff7e14556d8273a0723b3bc892c
Fabian Grassold <fabian.grassold@br.de>
2017-07-21 13:49:20 +0200

    changed ps command to yeild results (ubuntu specific?)

--- td-agent_exporter.go
+++ td-agent_exporter.go
@@ -161,7 +161,7 @@ func (e *Exporter) resolveTdAgentId() (map[string]string, error) {
 }
 
 func (e *Exporter) execPsCommand() (string, error) {
-	ps, err := exec.Command("ps", "-C", "ruby", "-f").Output()
+	ps, err := exec.Command("ps", "-e", "-f").Output()
 	if err != nil {
 		log.Error(err)
 		return "", err
